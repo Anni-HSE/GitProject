544cd513b78a4dd30f9813fa080dc5e0fce85261
Popov Nikita <58226087+Inoskeee@users.noreply.github.com>
2021-10-05 12:41:36 +0500

    Leon vs El_Primo

--- GitProject/Program.cs
+++ GitProject/Program.cs
@@ -6,12 +6,23 @@ namespace GitProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BrawlStars leon = new BrawlStars("Leon", 2250);
+            BrawlStars elPrimo = new BrawlStars("El Primo", 4250);
+            Console.WriteLine($"КТО КРУЧИ?? {leon.Brawlik} или {elPrimo.Brawlik}");
+            leon.Health > elPrimo.Health ? Console.WriteLine(leon.Brawlik) : Console.WriteLine(elPrimo.Brawlik);
+
         }
     }
 
     class BrawlStars
     {
-        string Brawlik { get; set; }
+        public string Brawlik { get; set; }
+        public int Health { get; set; }
+
+        public BrawlStars(string nameHero, int healthHero)
+        {
+            Brawlik = nameHero;
+            Health = healthHero;
+        }
     }
 }
